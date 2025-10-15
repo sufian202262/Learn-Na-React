30fd419c922e26541a26ad58d8b52c8956246a83
sufian201462 <38572598+sufian201462@users.noreply.github.com>
2025-10-15 16:18:08 +0600

    episode - 1 done

---
+++ App.js
@@ -0,0 +1,12 @@
+const nested = React.createElement("div",{id:"parent"},
+                [React.createElement("div", {id:"child1"},
+                    React.createElement("h1",{},"I am heading one")
+                ),
+                React.createElement("div", {id:"child2"},
+                    React.createElement("h2",{},"I am heading two")
+                ),
+            ]
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(nested);
