3ff73ee4beb70f8576f68fec95c411e4c3506915
sufian201462 <38572598+sufian201462@users.noreply.github.com>
2025-10-17 17:08:03 +0600

    Element, Component

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const nested = React.createElement("div",{id:"parent"},
-                [React.createElement("div", {id:"child1"},
-                    React.createElement("h1",{},"I am heading one")
-                ),
-                React.createElement("div", {id:"child2"},
-                    React.createElement("h2",{},"I am heading two")
-                ),
-            ]
+//React element
+const heading = (
+    <div>
+        <h1>hello world</h1>
+        <p>this is paragraph here</p>
+    </div>
+)
+
+//React Component
+//1. class based component - old
+//2. functional component - new
+
+const Title = () => (
+    <h1 id="heading">This is title</h1>
+)
+const Heading2 = () => (
+    <div>
+        {Title()}
+        <Title/>
+        <Title></Title>
+        <h2>hello world 2</h2>
+    </div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(nested);
+
+root.render(<Heading2/>);
